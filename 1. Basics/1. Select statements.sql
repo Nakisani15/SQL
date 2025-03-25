1bba3b697662c021beb593d73825e1761ff08b0d
Nakisani15 <nakineluheni@gmail.com>
2025-03-25 15:04:50 +0200

    COMMIT

--- 1. Basics/1. Select statements.sql
+++ 1. Basics/1. Select statements.sql
@@ -1,6 +1,30 @@
-SELECT * FROM Sales.SalesOrder
-WHERE Status = 'Shipped'
-GROUP BY OrderDate 
-HAVING COUNT(OrderID) > 1
-SELECT OrderDate, COUNT(OrderID) AS Orders
-ORDER BY OrderDate DESC;
+--SELECTING EVERYTHING IN A TABLE--
+Select * FROM Production.Product
+
+SELECT * FROM Person.Person
+WHERE Title = 'Mrs'
+
+SELECT BusinessEntityID, JobTitle, Birthdate
+FROM HumanResources.Employee
+WHERE MaritalStatus = 'M'
+
+
+SELECT PersonType, NameStyle, Title
+FROM Person.Person
+ORDER BY ModifiedDate DESC;
+
+--SELECT/TOP--
+SELECT TOP 5 *  
+FROM HumanResources.Employee  
+ORDER BY HireDate DESC; 
+
+--OR--
+SELECT *  
+FROM HumanResources.Employee  
+ORDER BY HireDate DESC  
+OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY;
+
+--SELECT COUNT--
+SELECT COUNT(*) FROM HumanResources.Employee
+WHERE JobTitle = 'Tool Designer';
+
